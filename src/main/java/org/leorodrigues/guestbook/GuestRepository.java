64c1c096362a632791d7860dd239eb6aa1837059
Leo Rodrigues <leorodriguesrj@gmail.com>
2020-11-10 11:37:38 -0300

    switched spring boot version

--- src/main/java/org/leorodrigues/guestbook/GuestRepository.java
+++ src/main/java/org/leorodrigues/guestbook/GuestRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "guests", path = "guests")
-public interface PersonRepository extends MongoRepository<Guest, String> {
+public interface GuestRepository extends MongoRepository<Guest, String> {
     List<Guest> findAll();
 }
